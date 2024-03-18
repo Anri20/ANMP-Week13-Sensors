30d50e2d340e17839a71dd1c535ed444cd985d88
Anri20 <88647934+Anri20@users.noreply.github.com>
2024-03-18 11:31:41 +0700

    Implement Accelerometer Sensor

--- app/src/main/java/com/example/week13/MainActivity.kt
+++ app/src/main/java/com/example/week13/MainActivity.kt
@@ -1,15 +1,67 @@
 package com.example.week13
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.week13.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var sensorManager: SensorManager
+    private var accelerometerReading = FloatArray(3)
+
+    private var accelerometerSensor: Sensor? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (accelerometerSensor != null) {
+            Toast.makeText(this, "Accelerometer Sensor detected", Toast.LENGTH_SHORT).show()
+            sensorManager.registerListener(
+                this,
+                accelerometerSensor,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        } else {
+            Toast.makeText(this, "Accelerometer Sensor detected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        p0?.let {
+            when (it.sensor.type) {
+                Sensor.TYPE_ACCELEROMETER -> {
+                    accelerometerReading= it.values
+                    var x = it.values[0]
+                    var y = it.values[1]
+                    var z = it.values[2]
+                    binding.txtAccelerometer.text = "x: $x, y: $y, z: $z"
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("Not yet implemented")
     }
 }
